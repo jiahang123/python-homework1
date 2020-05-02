c5ba9e06369db1c8782c28bb7a05fd121018f36a
dongyanwen <yanwen_donge@163.com>
2020-05-02 13:07:04 +0800

    123

---
+++ 面向对象.py
@@ -0,0 +1,14 @@
+class Person():
+    name="noname"
+    def get_name(self):
+        return  self.name
+print(Person.name)
+p=Person()
+print(p.name)
+print(p.get_name())
+# p.name="lili"
+p.name="xiaoming"
+Person.name="lili"
+print(p.name)
+p1=Person()
+print(p1.name)
