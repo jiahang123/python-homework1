bbb99e3a6acf0bb7547577afc1de106052367280
dongyanwen <yanwen_donge@163.com>
2020-05-02 13:12:52 +0800

    123

--- 面向对象.py
+++ 面向对象.py
@@ -1,14 +1,17 @@
 class Person():
-    name="noname"
+    name = "noname"
+
     def get_name(self):
-        return  self.name
+        return self.name
+
+
 print(Person.name)
-p=Person()
+p = Person()
 print(p.name)
 print(p.get_name())
 # p.name="lili"
-p.name="xiaoming"
-Person.name="lili"
+p.name = "xiaoming"
+Person.name = "lili"
 print(p.name)
-p1=Person()
+p1 = Person()
 print(p1.name)
--- 面向对象实例化.py
+++ 面向对象实例化.py
@@ -1,24 +1,28 @@
 class Person:
     # def _init_(self):
-    def __init__(self,name,age,gender):
-        self.name=name
-        self.age=age
-        self.gender=gender
-    def set_att(self,value):
-        self.value=value
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def set_att(self, value):
+        self.value = value
+
     def eat(self):
         print(f"name:{self.name},age:{self.age},gender:{self.gender} 我在吃")
+
     def drink(self):
         print(f"name:{self.name},age:{self.age},gender:{self.gender} 我在喝")
+
     def run(self):
         print(f"name:{self.name},age:{self.age},gender:{self.gender} 我在跑")
-xiaoming=Person("xiaoming",10,'male')
-xiaohong=Person("xiaohong",15,'famale')
+
+
+xiaoming = Person("xiaoming", 10, 'male')
+xiaohong = Person("xiaohong", 15, 'famale')
 
 print(xiaoming.name)
 xiaohong.run()
 
 xiaoming.set_att("fit")
 print(xiaoming.value)
-
-webdriver
